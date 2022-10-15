79783fcae5c76a75539c8551d421922acbf3fb91
altinbasMert <lordonmert@gmail.com>
2022-10-15 23:23:28 +0300

    Shopping App with Jetpack Compose

--- app/src/main/java/com/info/shoppingapp/presentation/tiles/product/SizeListTile.kt
+++ app/src/main/java/com/info/shoppingapp/presentation/tiles/product/SizeListTile.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
